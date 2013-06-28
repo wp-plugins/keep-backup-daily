61b9a255e453bb6bc4f6111912299df7c3b34841
fahadmahmood <fahadmahmood@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-28 17:53:01 +0000

    Version 1.4.8
    git-svn-id: https://plugins.svn.wordpress.org/keep-backup-daily/tags/1.4.8@733680 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kbd_settings.php
@@ -0,0 +1,240 @@
+<style type="text/css">
+.kbd_settings a
+{
+	cursor:pointer;
+}
+
+
+.kbd_settings medium.expected
+
+
+{
+
+
+	float:right;
+
+
+}
+
+
+.kbd_settings .useful_link{
+
+	margin-left:10px;
+	float:right;
+}
+.kbd_settings .flower_img{
+	float: left;
+    left: -6px;
+    position: relative;
+    top: 12px;
+    width: 3%;
+}	
+
+.kbd_settings ul.kbd_rc  li.tick,
+.kbd_settings ul.kbd_rc  li.cross{
+    background: transparent url(<?php echo plugins_url('tick_cross.png', __FILE__ ); ?>) no-repeat 0px 3px;
+    list-style-type: none;   
+    padding: 0 0 0 22px;
+}
+.kbd_settings ul.kbd_rc  li.cross{
+	background-position:0px -14px;
+}
+
+.kbd_settings .hide{
+	display:none;
+}
+</style>
+
+
+<div class="wrap kbd_settings">
+
+<div class="icon32" id="icon-options-general"><br></div><h2>Keep Backup Daily - Settings</h2>
+
+<?php echo $settings['notification']; $wpurl = get_bloginfo('wpurl'); ?>
+
+<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+
+
+<div class="welcome-panel" id="request_panel">
+<a class="welcome-panel-close dismiss_link">Dismiss</a>
+<img class="flower_img" src="<?php echo plugin_dir_url(__FILE__); ?>/flower.png">
+
+		<h2><span class="promo">Show some love!</span></h2>
+		<p>Want to appreciate the effort behind this plugin? <a target="_blank" class="button" href="http://www.websitedesignwebsitedevelopment.com/donate-now/" >Donate</a> $5, $10 or $50 now!</p>
+       
+        <p>Or you could:</p><ul><li><a href="http://wordpress.org/support/view/plugin-reviews/keep-backup-daily" target="_blank">Rate this plugin 5 stars on WordPress.org</a></li></ul>	
+</div>
+
+<input type="hidden" name="kbd_key" value="<?php echo $settings['kbd_key']; ?>">
+
+<table class="form-table">
+
+<tbody><tr valign="top">
+
+<th scope="row">Backup Required</th>
+
+<td id="front-static-pages"><fieldset>
+
+	<p><label for="cron_d">
+
+		<input type="radio" <?php echo ($settings['backup_required']=='cron_d'?'checked="checked"':''); ?> class="tog" id="cron_d" value="cron_d" name="backup_required">Daily</label>
+
+
+		<?php echo $settings['cron_d']['expected_backup']; ?>
+
+
+	</p>
+
+	<p><label for="cron_w">
+
+		<input type="radio" <?php echo ($settings['backup_required']=='cron_w'?'checked="checked"':''); ?> class="tog" id="cron_w" value="cron_w" name="backup_required">Weekly</label>
+
+
+		<?php echo $settings['cron_w']['expected_backup']; ?>
+
+
+	</p>
+
+	<p><label for="cron_m">
+
+		<input type="radio" <?php echo ($settings['backup_required']=='cron_m'?'checked="checked"':''); ?> class="tog" id="cron_m" value="cron_m" name="backup_required">Monthly</label>
+
+
+		<?php echo $settings['cron_m']['expected_backup']; ?>
+
+
+	</p>    
+
+	<p><label for="cron_y">
+
+		<input type="radio" <?php echo ($settings['backup_required']=='cron_y'?'checked="checked"':''); ?> class="tog" id="cron_y" value="cron_y" name="backup_required">Yearly</label>
+
+
+		<?php echo $settings['cron_y']['expected_backup']; ?>
+
+
+	</p> 
+	<p>
+    <a id="cron_now" title="Click here to backup now">Click here to backup now</a>
+    </p>
+    
+</fieldset></td>
+
+<td>
+<div class="hide">
+Requirements Console:
+<ul class="kbd_rc">
+	<li <?php echo $kbd_rc['is_writable']?'class="tick"':'class="cross"';?>> Write Permissions</li>
+    <li <?php echo $kbd_rc['ZipArchive']?'class="tick"':'class="cross"';?>>Zip Library</li>  
+    <li <?php echo $kbd_rc['mcrypt_create_iv']?'class="tick"':'class="cross"';?>>MCRYPT Library</li>
+</ul>
+</div>
+</td>
+
+</tr>
+
+<tr valign="top">
+
+<th scope="row"><label for="recpient_email_address">Recipient Email Address</label></th>
+
+<td colspan="2">
+
+<input type="text" class="medium-text" value="<?php echo is_array($settings['recpient_email_address'])?implode(',', $settings['recpient_email_address']):$settings['recpient_email_address']; ?>" step="1" name="recpient_email_address" id="recpient_email_address">
+
+<p class="description">Default: <?php echo $default_email; ?></p>
+
+</td></tr>
+
+<tr valign="top">
+
+<th scope="row">Maintain Log</th>
+
+<td colspan="2"><fieldset>
+
+	<label for="maintain_log"><input type="checkbox" value="1" <?php echo ($settings['maintain_log']==1?'checked="checked"':''); ?> name="maintain_log" id="maintain_log">
+
+	You will be able to view log with date and time.</label>
+
+	<p class="description">Only log file will be stored on your server.</p>
+
+    
+
+    <p class="description">
+
+    <?php if($settings['log']!=''): ?>
+
+    <div style="height:160px; background-color:#F3F3F3; overflow:auto; width:64%;">
+
+    <?php echo nl2br($settings['log']); ?>
+
+    </div>
+
+    <?php endif; ?>
+
+    </p>
+
+</fieldset></td>
+
+</tr>
+
+<tr valign="top">
+
+<th scope="row">Cron Job Settings <span title="By default we will access cron file placed on your server for your convenience. Because most of the users don't have idea that how to set a cron or conscious about their server performance." style="color:red">(Important)</span></th>
+
+<td colspan="2"><fieldset>
+
+	<p><label for="kbd_cron_default">
+
+		<input <?php echo ($settings['cron_server']=='default'?'checked="checked"':''); ?> type="radio" class="tog" id="kbd_cron_default" value="default" name="cron_server">Default</label>
+
+	</p>
+
+    <p><label for="kbd_cron_custom">
+
+		<input <?php echo ($settings['cron_server']=='custom'?'checked="checked"':''); ?> type="radio" class="tog" id="kbd_cron_custom" value="custom" name="cron_server">Custom <a>(more)</a></label>
+
+	</p>
+
+    <p class="description cron_line" style="display:none">You have to run the following file, write the cron job command which is suitable on your server. <input type="text" class="large-text" value="<?php echo $wpurl; ?>/?kbd_cron_process=1"></p>
+
+</fieldset></td>
+
+</tr>
+</tbody></table>
+
+<p class="submit"><input type="submit" value="Save Changes" class="button button-primary" id="submit" name="submit"><a class="useful_link">Find this plugin useful?</a></p></form>
+
+</div>
+
+<script type="text/javascript" language="javascript">
+jQuery(document).ready(function($) {
+
+jQuery('.dismiss_link').click(function(){
+		jQuery(this).parent().slideUp();
+		jQuery('.useful_link').fadeIn();
+	});
+	jQuery('.useful_link').click(function(){
+		jQuery('.dismiss_link').parent().slideDown();
+		jQuery(this).fadeOut();
+	});
+
+jQuery('#kbd_cron_custom').parent().find('a').click(function(){
+
+	jQuery('.cron_line').toggle();
+
+});jQuery('#cron_now').click(function(){
+
+	jQuery(this).parent().append('<p class="sending_backup">Sending to '+jQuery('#recpient_email_address').val()+'</p>');
+	jQuery(this).remove();
+	
+	var jqxhr = jQuery.get(jQuery('.cron_line input').val(), function() {
+	
+	})
+	.done(function() { jQuery('.sending_backup').html('Successfully sent.'); })
+	.fail(function() { jQuery('.sending_backup').html('Failed.'); })
+	.always(function() { jQuery('.sending_backup').html('Please check your inbox.'); });
+
+	
+});
+});	
+</script>
