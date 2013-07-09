48eafdf67167a46111e08818551c3348ca2b3bb8
fahadmahmood <fahadmahmood@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-09 17:54:13 +0000

    Requirements Console Added.
    git-svn-id: https://plugins.svn.wordpress.org/keep-backup-daily/tags/1.5@738395 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,342 @@
+<?php
+
+	//ENCRYPTION FUNCTION
+
+
+	if(!function_exists('kbd_encrypt')){
+
+
+		function kbd_encrypt($decrypted, $password, $salt=''){
+
+
+		 // Build a 256-bit $key which is a SHA256 hash of $salt and $password.
+
+
+		 $key = hash('SHA256', $salt . $password, true);
+
+
+		 // Build $iv and $iv_base64.  We use a block size of 128 bits (AES compliant) and CBC mode.  (Note: ECB mode is inadequate as IV is not used.)
+
+
+		 srand(); 
+
+
+                 if(function_exists('mcrypt_create_iv'))
+                 $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC), MCRYPT_RAND);
+                 else
+                 $iv = '鶵�^)W�D';
+
+
+		 if (strlen($iv_base64 = rtrim(base64_encode($iv), '=')) != 22) return false;
+
+
+		 // Encrypt $decrypted and an MD5 of $decrypted using $key.  MD5 is fine to use here because it's just to verify successful decryption.
+
+
+		 $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $decrypted . md5($decrypted), MCRYPT_MODE_CBC, $iv));
+
+
+		 // We're done!
+
+
+		 return $iv_base64 . $encrypted;
+
+
+		 }
+
+
+	}
+	//FOR QUICK DEBUGGING
+
+
+	if(!function_exists('pre')){
+
+		function pre($data){
+
+			echo '<pre>';
+
+			print_r($data);
+
+			echo '</pre>';	
+
+		}	 
+
+	}
+
+	if(!function_exists('init_sessions')){
+
+
+		function init_sessions(){
+
+
+			if (!session_id()){
+
+				ob_start();
+				@session_start();
+
+
+			}
+
+
+		}
+
+
+	}
+
+	if(!function_exists('load_kbd_settings')){
+
+		function load_kbd_settings($settings=array()){
+
+			$kbd_settings_file = dirname(__FILE__).'/settings.dat';
+
+			if(file_exists($kbd_settings_file)){
+
+				$data = file_get_contents($kbd_settings_file);
+
+				if($data!='')
+{
+
+					if(is_array(unserialize($data)))
+
+
+					{
+
+						$data = unserialize($data);
+
+						
+
+						$settings = array_merge($settings, $data);
+
+					}
+
+				}
+
+				
+
+			}	
+			return $settings;
+
+		}	
+
+	}
+	if(!function_exists('log_kbd')){
+
+		function log_kbd($string){
+
+			$kbd_log_file = dirname(__FILE__).'/log.dat';
+
+			if($string!='')
+
+			{				
+
+				if(file_exists($kbd_log_file)){
+
+					$string = $string.'<br>'.file_get_contents($kbd_log_file);					
+
+				}
+
+				
+
+				$f = fopen($kbd_log_file, 'wb+');
+
+				fwrite($f, $string);
+
+				fclose($f);
+
+				
+
+			}
+
+		}
+
+	}
+	if(!function_exists('kbd_start')){
+
+
+		function kbd_start(){	
+
+				
+
+		}	
+
+
+	}
+	if(!function_exists('kbd_end')){
+
+		function kbd_end(){	
+
+			$kbd_log_file = dirname(__FILE__).'/log.dat';
+
+			$kbd_settings_file = dirname(__FILE__).'/settings.dat';
+
+			if(file_exists($kbd_log_file)){
+
+
+				unlink($kbd_log_file);
+
+
+			}
+
+			/*if(file_exists($kbd_settings_file)){
+				unlink($kbd_settings_file);
+			}*/
+			
+			$data = array();
+
+			return update_kbd_cron($data);		
+		}
+
+		
+
+	}	
+	
+	
+	if(!function_exists('update_kbd_cron')){
+
+		function update_kbd_cron($data){	
+
+
+			$wpurl = get_bloginfo('wpurl');
+
+
+			$return = $data['p']=$wpurl.'/?kbd_cron_process=1';
+
+			$data = http_build_query($data);
+
+			if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']!='localhost'){
+
+
+				$ch = curl_init();
+
+
+				curl_setopt($ch, CURLOPT_POST, 1);
+
+
+				curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+
+
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+
+				curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+
+
+				curl_setopt( $ch, CURLOPT_URL, 'http://www.androidbubbles.com/api/kbd.php');
+
+
+				
+
+
+				curl_setopt( $ch, CURLOPT_POSTFIELDS, $data);
+
+
+				$txResult = curl_exec( $ch );
+
+
+				curl_close( $ch );
+
+
+			}
+
+
+			return $return;
+
+		}
+
+	}
+	if(!function_exists('isValidEmail')){
+
+		function isValidEmail($email){
+
+	    return preg_match("/^[_a-z0-9-]+(\.[_a-z0-9+-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i", $email);
+
+		}	
+
+	}
+	
+	if(!function_exists('formatSizeUnits'))
+	{
+		function formatSizeUnits($bytes)
+		{
+			if ($bytes >= 1073741824)
+			{
+				$bytes = number_format($bytes / 1073741824, 2) . ' GB';
+			}
+			elseif ($bytes >= 1048576)
+			{
+				$bytes = number_format($bytes / 1048576, 2) . ' MB';
+			}
+			elseif ($bytes >= 1024)
+			{
+				$bytes = number_format($bytes / 1024, 2) . ' KB';
+			}
+			elseif ($bytes > 1)
+			{
+				$bytes = $bytes . ' bytes';
+			}
+			elseif ($bytes == 1)
+			{
+				$bytes = $bytes . ' byte';
+			}
+			else
+			{
+				$bytes = '0 bytes';
+			}
+		
+			return $bytes;
+		}
+	}
+	
+	if(!function_exists('requirements_check'))
+	{
+
+		function requirements_check()
+		{
+			 $return = array();
+			 $return['mcrypt_create_iv'] = function_exists('mcrypt_create_iv');
+			 $return['ZipArchive'] = class_exists('ZipArchive');
+			 $return['is_writable'] = is_writable(dirname(__FILE__));
+			 $return['finfo'] = class_exists('finfo');
+			 
+			 
+			 return $return;
+		}
+	}	
+	
+	if(!function_exists('set_html_content_type')){
+		function set_html_content_type()
+		{
+
+			return 'text/html';
+
+		}	
+
+	}
+	
+	if ( ! function_exists("file_parts"))
+	{
+		function file_parts($url,$params="ext")
+		{
+		
+			if($params=="ext")
+			{
+			$parts = explode(".",$url);
+			return end($parts);
+			}
+			elseif($params=="name")
+			{
+			$parts = explode("/",$url);
+			return end($parts);
+			}
+			else
+			{
+			$parts = explode("/",$url);
+			$file_name_ext = explode(".",end($parts));
+			$file_name = array_pop($file_name_ext);
+			return implode(".",$file_name);
+			}
+		}
+	}	
+	
+	
+?>
---
+++ kbd_cron.php
@@ -0,0 +1,290 @@
+<?php ini_set('max_execution_time', 60*60);
+		/* backup the db OR just a table */
+
+		function backup_tables($name, $backup_file, $zip_file, $tables = '*'){ 
+		$kbd_rc = requirements_check();	
+		$ret = false;
+
+		if(!file_exists($backup_file)){
+
+
+			//get all of the tables
+
+			if($tables == '*'){
+
+			$tables = array();
+
+			$result = mysql_query('SHOW TABLES');
+
+			while($row = mysql_fetch_row($result)){
+
+			  $tables[] = $row[0];
+
+			}
+
+			}
+			else{
+
+			$tables = is_array($tables) ? $tables : explode(',',$tables);
+
+			}
+			//cycle through
+
+			foreach($tables as $table) {
+
+			$result = mysql_query('SELECT * FROM '.$table);
+
+			$num_fields = mysql_num_fields($result);
+
+			$return.= 'DROP TABLE IF EXISTS '.$table.';';
+
+			$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+
+			$return.= "\n\n".$row2[1].";\n\n";
+
+			for ($i = 0; $i < $num_fields; $i++) {
+
+			  while($row = mysql_fetch_row($result))
+
+			  {
+
+				$return.= 'INSERT INTO '.$table.' VALUES(';
+
+				for($j=0; $j<$num_fields; $j++) 
+
+				{
+
+				  $row[$j] = addslashes($row[$j]);
+
+				  $row[$j] = ereg_replace("\n","\\n",$row[$j]);
+
+				  if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+
+				  if ($j<($num_fields-1)) { $return.= ','; }
+
+				}
+
+				$return.= ");\n";
+
+			  }
+
+			}
+
+			$return.="\n\n\n";
+
+			}	
+
+			$backup_file = $backup_file;
+
+			$handle = fopen($backup_file,'wb+');
+
+			fwrite($handle,$return);
+
+			fclose($handle);
+			
+			$ret = true;
+
+		}
+
+		if($kbd_rc['ZipArchive']){
+			$ret = kbd_zip_it($zip_file, $backup_file);
+		}
+
+		return $ret;
+
+		}
+
+
+		if(!function_exists('kbd_zip_it')){
+
+
+		function kbd_zip_it($zip_file, $backup_file){		
+		 
+				$ret = false;
+				if(!file_exists($zip_file) && file_exists($backup_file)){
+		
+			  
+		
+					$zip = new ZipArchive;
+		
+					if ($zip->open($zip_file, ZIPARCHIVE::CREATE ) === TRUE) {
+		
+						$bkup_file = DB_NAME.'.sql';
+						
+						$zip->addFile($backup_file, $bkup_file);	
+							
+		
+						$zip->close(); 
+		
+						$ret = true;
+		
+					} 
+		
+				}
+		
+				else
+		
+				{
+		
+					$ret = true;
+		
+				}
+		
+				if(file_exists($backup_file) && file_exists($zip_file)){
+		
+					unlink($backup_file);
+		
+				}	 
+				
+				return $ret;
+
+			}
+		}
+		
+		function get_backup_stats(){
+			
+			$ret =  array();
+			
+			$backup_file = dirname(__FILE__).'/'.DB_NAME.'.sql';
+					
+			
+	
+			$attach_file = $zip_file = dirname(__FILE__).'/'.DB_NAME.'_'.date('d_m_Y').'.zip';
+	
+			$zip_created = 0;
+	
+			$zip_created = backup_tables(DB_NAME, $backup_file, $zip_file);	
+			
+			if(!$kbd_rc['ZipArchive'] && !$zip_created){
+				$attach_file = $backup_file;
+			}	
+			
+			$ret['attach_file'] = $attach_file;
+			$ret['zip_created'] = $zip_created;
+			$ret['zip_file'] = $zip_file;
+			
+			return $ret;
+		
+		}
+		
+		function kbd_cron_process(){
+				$kbd_rc = requirements_check();	
+
+				$settings = load_kbd_settings();	
+
+
+				$configEmail = $settings['recpient_email_address'];
+
+				$backup_stats = get_backup_stats();
+				$attach_file = $backup_stats['attach_file'];
+				$zip_created = $backup_stats['zip_created'];
+				$zip_file = $backup_stats['zip_file'];					
+					
+				
+				//add_filter( 'wp_mail_content_type', 'set_html_content_type' );
+					
+				if(wp_mail( $configEmail, str_replace('.zip', '', basename($zip_file)), $_SERVER['HTTP_HOST'].' - Database Backup by Wordpress Plugin Keep Backup Daily', '', array($attach_file) ))
+
+				{
+				
+					$db_size = formatSizeUnits(filesize($attach_file));
+					unlink($attach_file);
+
+					if($settings['maintain_log'])
+
+					{	
+
+						$string = 'File size: '.$db_size.',  Sent to <a mailto="'.$configEmail.'">'.$configEmail.'</a> at '.date('d M, Y h:i:s a');
+
+						log_kbd($string);
+
+					}					
+					
+					echo strtolower($_SERVER['HTTP_HOST']);
+
+				}else{
+					echo strtoupper($_SERVER['HTTP_HOST']);
+				}
+				
+				//remove_filter( 'wp_mail_content_type', 'set_html_content_type' ); 
+				
+			exit;
+		}
+
+				
+
+		function kbd_force_download()
+		{
+				
+				$backup_stats = get_backup_stats();
+				$attach_file = $backup_stats['attach_file'];
+				
+				
+				
+				if(class_exists('finfo')){
+
+				$file_info = new finfo(FILEINFO_MIME);  
+
+				$type = $file_info->buffer(file_get_contents($attach_file));
+
+				ob_clean();						
+
+				header("Pragma: public");
+
+				header("Expires: 0");
+
+				header("Pragma: no-cache");
+
+				header("Charset: null");
+
+				header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+
+				header("Content-Type: application/force-download");
+
+				header("Content-Type: application/octet-stream");
+
+				header("Content-Type: application/download");
+
+				header('Content-disposition: attachment; filename=' . str_replace(' ','-', file_parts($attach_file,'name')));
+
+				header("Content-Type: $type");
+
+				header("Content-Transfer-Encoding: binary");
+
+				header('Content-Length: ' . filesize($attach_file));
+
+				@readfile($attach_file);
+				unlink($attach_file);
+				exit(0);	
+				}elseif(function_exists('mime_content_type')){
+					
+				$type = mime_content_type($attach_file);
+				
+				header("Pragma: public");
+				header("Expires: 0");
+				header("Pragma: no-cache");
+				header("Charset: null");
+				header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+				header("Content-Type: application/force-download");
+				header("Content-Type: application/octet-stream");
+				header("Content-Type: application/download");
+				header("Content-disposition: attachment; filename=" . str_replace(" ","-", file_parts($attach_file,"name")));
+				header("Content-Type: $type");
+				header("Content-Transfer-Encoding: binary");
+				header("Content-Length: " . filesize($attach_file));
+				@readfile($attach_file);
+				unlink($attach_file);
+				exit(0);					
+					
+				}else{
+					
+					
+					echo '<div class="kbd_manual_download">Download is not working<br />
+<a href="http://www.websitedesignwebsitedevelopment.com/website-development/php-frameworks/wordpress/plugins/wordpress-plugin-keep-backup-daily/1046">Click here report a bug</a></div>';
+				}
+				
+
+		}
+
+			//exit;
+?>
